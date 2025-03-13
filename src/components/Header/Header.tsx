0565f1361c92202d4afd3ccc920417992b20de2b
Lynsie Aragon <lynsie.aragon@gmail.com>
2025-03-13 14:59:21 -0400

    Rough nav bar layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import NavBar from "../NavBar/NavBar";
 function Header() {
   return (
     <div className="bg-green-100">
-      <div className="text-[#d34723] font-bold text-5xl text-center p-8">
+      <div className="text-[#d34723] font-bold text-5xl text-center p-8 border border-b-[#d34723] border-b-solid border-transparent border-b-4">
         Lynsie Aragon
       </div>
       <NavBar />
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,8 +1,8 @@
 function NavBar() {
   return (
-    <ul className="flex flex-row">
-      <li>About Me</li>
-      <li>Professional Experience & Skills</li>
+    <ul className="flex flex-row py-4 gap-12 justify-center">
+      <li>About</li>
+      <li>Experience</li>
       <li>Bowling</li>
       <li>Contact</li>
     </ul>
